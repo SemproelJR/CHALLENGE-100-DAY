0af63e7517f3d556f2904b98818f1f251bbbbc76
Tomi Irawan <tomi.irawan120.com>
2023-01-23 12:15:31 +0700

    modif main anonymous

--- latihan_anonymous_method/lib/main.dart
+++ latihan_anonymous_method/lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
